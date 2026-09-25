ccf69afdc2abbb0ab15c0b62c4b61f5bef1892ef
JesseCoretta <74126726+JesseCoretta@users.noreply.github.com>
2026-09-25 01:41:54 -0700

    LDAPOID.Equal

--- oid.go
+++ oid.go
@@ -103,7 +103,7 @@ func isNumericOID(x any) bool {
 	return err == nil
 }
 
-func (r LDAPOID) Equal(o LDAPOID) bool { return beq(r,o) }
+func (r LDAPOID) Equal(o LDAPOID) bool { return beq(r, o) }
 
 func (r LDAPOID) String() string { return string(r) }
 
